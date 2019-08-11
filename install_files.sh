9ea2a40deb72a943618cde9a42a82f0b518ea62b
btandersen383 <beathan.andersen@gmail.com>
2019-08-11 11:25:16 -0500

    filled in install copy script

--- install_files.sh
+++ install_files.sh
@@ -3,4 +3,14 @@
 # used to copy files from staging to sublime package dir
 # I do this to keep changes from immediately affecting sublime
 
-echo not imp
+echo "copying files..."
+
+DEST=~/.config/sublime-text-3/Packages/MeeseeksSVN
+cp -r icons/ $DEST
+cp -r keymaps/ $DEST
+cp -r lib/ $DEST
+cp -r menus/ $DEST
+cp -r syntax/ $DEST
+cp "Meeseeks.sublime-settings" $DEST
+cp "MeeseeksSVN.sublime-commands" $DEST
+cp "SVNCommands.py" $DEST
